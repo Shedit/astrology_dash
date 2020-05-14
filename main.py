2603525d03c49590725ea24918f4d664f21744fa
Shedit <pyrt.91@gmail.com>
2020-05-14 17:29:48 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
+# fig.add_trace( ... )
+# fig.update_layout( ... )
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+app.run_server(debug=True, use_reloader=False)
